2bebd18b4f7092f4741905c8220d32f4ebaf7feb
marcin <marcin_dolega@epam.com>
2022-07-21 12:20:35 +0200

    0. Initial Commit

---
+++ src/main/java/pl/dolega/mybookdb/MyBookdbApplication.java
@@ -0,0 +1,13 @@
+package pl.dolega.mybookdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dolega/mybookdb/MyBookdbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolega.mybookdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
